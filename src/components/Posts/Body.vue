8e98ffae2ad58e9292c2c05cde27f28083de816a
user.name like:"Lien Sanchez <user.email like:liensilviosanchez@gmail.com>
2024-11-01 02:23:33 -0300

    Check Prjects

---
+++ src/components/Posts/Body.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        post_content: {
+            type: Object,
+            default: () => ({
+                content: { rendered: "" },
+                title: { rendered: "" },
+            }),
+        },
+    },
+    computed: {
+        renderedContent() {
+            return this.post_content?.content?.rendered || "";
+        },
+    },
+};
+</script>
+
+<template>
+    <section class="body custom-container">
+        <h1>{{ post_content?.title?.rendered || "Loading..." }}</h1>
+        <div v-if="post_content" v-html="renderedContent"></div>
+        <div v-else>Loading content...</div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    text-align: center;
+    font-size: 64px;
+}
+</style>
